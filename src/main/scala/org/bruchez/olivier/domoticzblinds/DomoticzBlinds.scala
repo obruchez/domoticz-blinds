664503b7da24e8b5058ef87b34183307dc215150
Olivier Bruchez <olivier@bruchez.org>
2017-05-27 17:23:01 +0200

    first commit

---
+++ src/main/scala/org/bruchez/olivier/domoticzblinds/DomoticzBlinds.scala
@@ -0,0 +1,7 @@
+package org.bruchez.olivier.domoticzblinds
+
+object DomoticzBlinds {
+  def main(args: Array[String]): Unit = {
+    println("Hello, world!")
+  }
+}
---
+++ src/main/scala/org/bruchez/olivier/domoticzblinds/MeteoSuisse.scala
@@ -0,0 +1,12 @@
+package org.bruchez.olivier.domoticzblinds
+
+object MeteoSuisse {
+
+}
+
+/*
+http://www.meteosuisse.admin.ch/etc/designs/meteoswiss/ajax/location/105400.json
+
+http://www.meteosuisse.admin.ch/product/output/forecast-chart/version__20170527_0732/fr/105400.json
+
+ */
