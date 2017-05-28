d7a6d9f52817032e8058a0605809d1fb87d579cb
Olivier Bruchez <olivier@bruchez.org>
2017-05-28 18:37:16 +0200

    Add ideas/comments

--- src/main/scala/org/bruchez/olivier/domoticzblinds/DomoticzBlinds.scala
+++ src/main/scala/org/bruchez/olivier/domoticzblinds/DomoticzBlinds.scala
@@ -5,3 +5,13 @@ object DomoticzBlinds {
     println("Hello, world.")
   }
 }
+
+/*
+API: http://<username:password@>domoticz-ip<:port>/json.htm?api-call
+
+To add: watchdog that restart domoticz when frozen (frozen if no answer from API
+
+Sunrise/sunset: /json.htm?type=command&param=getSunRiseSet
+
+To add: temperature logger => local database or other format
+ */
